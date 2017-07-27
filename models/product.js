c90d26c7a27b1f4b41619db009c80dae6db1cf9c
Jordan Parker <letsgetjordan@gmail.com>
2017-07-27 16:16:04 +0100

    Added all shopify product object info from https://help.shopify.com/themes/liquid/objects/product

--- models/product.js
+++ models/product.js
@@ -2,18 +2,51 @@ import Schema from "warehouse";
 
 module.exports = function(ctx) {
   var Product = new Schema({
-    id: {type: Number, default: (Math.random() * (99999999 - 111111) + 111111)},
-    url: {type: String, default: ''},
-    title: {type: String, default: ''},
     available: {type: Boolean, default: true},
+    // collections
+    compare_at_price_max: {type: Number, default: 0},
+    compare_at_price_min: {type: Number, default: 0},
+    // compare_at_price_varies
     description: {type: String, default: ''},
+    // featured_image
+    // first_available_variant
+    // handle
+    // has_only_default_variant
+    id: {type: Number, default: (Math.random() * (99999999 - 111111) + 111111)},
     images: [
       {
         src: {type: String, default: ''},
-        alt: {type: String, default: ''}
+        alt: {type: String, default: ''},
+        id: {type: Number, default: (Math.random() * (99999999 - 111111) + 111111)}
       }
-    ]
-  });
+    ],
+    // options
+    // options_with_values
+    // price
+    // price_max
+    // price_min
+    // price_varies
+    // selected_variant
+    // selected_or_first_available_variant
+    // tags
+    // template_suffix
+    title: {type: String, default: ''},
+    // type
+    url: {type: String, default: ''},
+    // variants
+    vendor: {type: String, default: ''}
+  })
 
+  Product.virtual('compare_at_price_varies', function() {
+    if(this.compare_at_price_max !== this.compare_at_price_min) {
+      return true
+    }
+    return false
+  })
+
+  // Alias'
+  Product.virtual('content', function() {
+    return this.description;
+  })
 
 }
